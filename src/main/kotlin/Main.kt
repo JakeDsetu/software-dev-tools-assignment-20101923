c06e8f82bb6faf6b750b228560b6a4fad108c8f9
JakeDsetu <20101923@mail.wit.ie>
2025-11-20 11:34:15 +0000

    Add Update Functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -40,6 +40,7 @@ fun runMenu() {
         when (option) {
             1  -> addSong()
             2  -> listAllSongs()
+            3  -> updateSong()
             0  -> exitApp()
             else -> println("Invalid option entered: $option")
         }
@@ -68,6 +69,33 @@ fun addSong(){
 
 fun listAllSongs() {
     println(musicAPI.listAllSongs())
+}
+
+fun updateSong(){
+    listAllSongs()
+    if (musicAPI.numberOfSongs() > 0) {
+        //only ask the user to choose the song if songs exist
+        val indexToUpdate = readNextInt("Enter the index of the song to update: ")
+        if (musicAPI.isValidIndex(indexToUpdate)) {
+            val id = readNextInt("Enter the id of the song: ")
+            val musicTitle = readNextLine("Enter the name of the song: ")
+            val musicArtist = readNextLine("Enter the name of the artist: ")
+            val lengthInMins = readNextInt("Enter the length (in minutes): ")
+            val releaseYear = readNextInt("Enter the year of release: ")
+            val genre = readNextLine("Enter the genre:")
+            val isWrittenByArtist = readNextBoolean("Was the song written by the artist? True or false:")
+            val numberOfPublicStreams = readNextInt("Enter the number of Public Listens:")
+            val hasVideo = readNextBoolean("Does the song have a music video? True or false:")
+
+            if (musicAPI.updateSong(indexToUpdate, Music(id, musicTitle, musicArtist, lengthInMins, releaseYear, genre, isWrittenByArtist, numberOfPublicStreams, hasVideo))){
+                println("Update Successful")
+            } else {
+                println("Update Failed")
+            }
+        } else {
+            println("There are no notes for this index number")
+        }
+    }
 }
 
 fun exitApp() {
--- src/main/kotlin/controllers/MusicAPI.kt
+++ src/main/kotlin/controllers/MusicAPI.kt
@@ -13,14 +13,51 @@ class MusicAPI(serializerType: Serializer) {
             .joinToString (separator = "\n") { note ->
                 songs.indexOf(note).toString() + ": " + note.toString() }
 
+    fun isValidListIndex(index: Int, list: List<Any>): Boolean {
+        return (index >= 0 && index < list.size)
+    }
+
+    fun isValidIndex(index: Int) :Boolean{
+        return isValidListIndex(index, songs);
+    }
+
+    fun findSong(index: Int): Music? {
+        return if (isValidListIndex(index, songs)) {
+            songs[index]
+        } else null
+    }
+
     fun addMusic(music: Music) : Boolean {
         //code for adding a song with a unique id
         return songs.add(music)
     }
 
+    fun numberOfSongs(): Int {
+        return songs.size
+    }
+
     fun listAllSongs(): String =
         if (songs.isEmpty()) "No songs stored"
         else formatListString(songs)
+
+    fun updateSong(indexToUpdate: Int, music: Music?): Boolean {
+        val foundSong = findSong(indexToUpdate)
+
+        if ((foundSong != null) && (music != null)) {
+            foundSong.musicTitle = music.musicTitle
+            foundSong.musicArtist = music.musicArtist
+            foundSong.lengthInMins = music.lengthInMins
+            foundSong.releaseYear = music.releaseYear
+            foundSong.genre = music.genre
+            foundSong.isWrittenByArtist = music.isWrittenByArtist
+            foundSong.numberOfPublicStreams = music.numberOfPublicStreams
+            foundSong.hasVideo = music.hasVideo
+            return true
+        }
+
+        //if the song was not found, return false, indicating that the update was not successful
+        return false
+    }
 
     fun musicExists(musicId: Int): Music? {
         return songs.find { it -> it.id == musicId }
