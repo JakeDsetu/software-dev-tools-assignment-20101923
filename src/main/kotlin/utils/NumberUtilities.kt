330d218085642f731638b49ec7f0017e4c495774
JakeDsetu <20101923@mail.wit.ie>
2025-11-28 13:08:53 +0000

    add comments to models and utilities

--- src/main/kotlin/utils/NumberUtilities.kt
+++ src/main/kotlin/utils/NumberUtilities.kt
@@ -1,5 +1,8 @@
 package setu.ie.utils
 
+/**
+ * Checks if a number is within a valid range of numbers.
+ */
 fun validRange(numberToCheck: Int, min: Int, max: Int): Boolean {
     return numberToCheck in min..max
 }
--- src/main/kotlin/utils/ValidateCategory.kt
+++ src/main/kotlin/utils/ValidateCategory.kt
@@ -2,6 +2,9 @@ package setu.ie.utils
 
 val categories = setOf("Pop", "Rock", "Hip-Hop", "Dance", "Electronic", "Country")
 
+/**
+ * Checks that an entered string falls under a specified list of categories.
+ */
 fun isValidCategory(categoryToCheck: String?): Boolean {
     for (category in categories) {
         if (category.equals(categoryToCheck, ignoreCase = true)) {
